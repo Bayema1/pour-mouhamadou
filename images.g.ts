db4fb3e706d20f1bd5601696b2c5e8d072365a70
Bayema1 <116888510+Bayema1@users.noreply.github.com>
2022-12-20 21:47:39 +0100

    Mise à jour pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,48 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "mhmd":return img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . f f f f f . . . . . 
+. . . . . e e e e e . . . . . 
+. . . . . e f e e e . . . . . 
+. . . . . e e e e e . . . . . 
+. . . . f 1 1 1 1 1 f . . . . 
+. . . f f 1 1 1 1 1 f f . . . 
+. e f f . f f f f f . f f e . 
+. . . . . f 5 5 5 f . . . . . 
+. . . . . 1 1 1 1 1 . . . . . 
+. . . . . 1 1 . 1 1 . . . . . 
+. . . . . 1 1 . 1 1 . . . . . 
+. . . . . 1 1 . 1 1 . . . . . 
+. . . . . 1 1 . 1 1 . . . . . 
+. . . . . f f . f f . . . . . 
+. . . . . f f . f f . . . . . 
+. . . . f f f . f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
